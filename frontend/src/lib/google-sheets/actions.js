1aab731809f86dba8dfefc3adc48b8b7ca7db2a8
Tejas Gupta <tgupta39@wisc.edu>
2025-12-08 10:51:22 -0600

    Please work

--- frontend/src/lib/google-sheets/actions.js
+++ frontend/src/lib/google-sheets/actions.js
@@ -3,10 +3,18 @@
 import { google } from 'googleapis';
 
 const getGoogleSheetsClient = async () => {
+  let privateKey = process.env.GOOGLE_PRIVATE_KEY;
+  
+  if (privateKey.startsWith('"') && privateKey.endsWith('"')) {
+    privateKey = privateKey.slice(1, -1);
+  }
+  
+  privateKey = privateKey.replace(/\\n/g, '\n');
+  
   const auth = new google.auth.GoogleAuth({
     credentials: {
       client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      private_key: privateKey,
     },
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
   });
